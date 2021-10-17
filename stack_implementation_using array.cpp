d9cb9f7c902b0efd2af02af137f9900fe8bc9c2e
Anushka Sitoke <83336769+asitoke123@users.noreply.github.com>
2021-10-17 22:50:37 +0530

    Add files via upload

---
+++ stack_implementation_using array.cpp
@@ -0,0 +1,63 @@
+//write a program to implement stack using Array with push and pop operations on it.
+#include <iostream>
+using namespace std;
+#define SIZE 5
+
+
+int stack[SIZE];
+int TOP=-1;
+
+void display()
+{
+    if(TOP==-1)
+    cout<<"\n no element in the stack,stack is empty!";
+    else
+    {
+        for(int i=TOP;i>-1;i--)
+        cout<<"\n"<<stack[i];
+    }
+}
+
+void push(int val)
+{
+    if(TOP==SIZE-1)
+    cout<<"\n stack is now full";
+    else
+    {
+        TOP++;
+        stack[TOP]=val;
+        cout<<"\n Inserted element:"<<val;
+    }
+}
+int pop()
+{
+    if(TOP==-1)
+    cout<<"\n stack is empty";
+    else
+    {
+        int temp=stack[TOP];
+        TOP--;
+        return temp;
+    }
+}
+
+int main() {
+    //display();
+    //int a=5;
+    //cout<<"\n enter the element:";
+    //cin>>a;
+    int t;
+    display();
+    push(10);
+    push(20);
+    //push(30);
+    //push(40);
+    //push(50);
+    t=pop();
+    cout<<"\n element poped:"<<t;
+    display();
+    push(30);
+    display();
+	return 0;
+}
+
